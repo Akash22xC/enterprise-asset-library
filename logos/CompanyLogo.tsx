847ec37de41ee9664e1a836bd0d32bb8f6ec33c2
akashwiz <aksh.ajith@wizr.ai>
2025-08-13 17:02:12 +0530

    changes

---
+++ logos/CompanyLogo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const CompanyLogo = () => (
+  <svg width="120" height="40" xmlns="http://www.w3.org/2000/svg">
+    <rect width="120" height="40" fill="red"/>
+    <text x="60" y="25" font-size="16" text-anchor="middle" fill="white">Tenant C</text>
+  </svg>
+);
+git add .
+git commit -m "Add sample assets for Tenant C"
+git push origin tenantC-main
