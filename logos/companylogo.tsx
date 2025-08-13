141c893d8c8f2a678d17b0252dbf4d9fab15f4b0
akashwiz <aksh.ajith@wizr.ai>
2025-08-13 15:44:21 +0530

    Add sample assets for Tenant A

---
+++ logos/companylogo.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+export const CompanyLogo: React.FC = () => (
+  <svg width="120" height="40" xmlns="http://www.w3.org/2000/svg">
+    <rect width="120" height="40" fill="blue"/>
+    <text x="60" y="25" font-size="16" text-anchor="middle" fill="white">Tenant A</text>
+  </svg>
+);
